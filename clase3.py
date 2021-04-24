29da066920a1919dfe5c76003bb1d2f8338ce507
davidt712 <71663265@certus.edu.pe>
2021-04-24 11:02:07 -0500

    C3

--- clase3.py
+++ clase3.py
@@ -10,3 +10,35 @@ a= 14
 b= 12
 c = a + b
 print(a, "hola", b, '=', c)
+print(a+b)
+
+
+h = "hola"
+i = "="
+
+print(str(a)+ h+ str(b)+ i+ str(c))
+
+s = "Kilometer"
+print(s.lower())
+
+
+a=1
+b=0
+print("a=", a, " b= ", b)
+print("a and b -->", a and b)
+print("a or b -->", a or b)
+
+a=23
+print("a=23 -->", a)
+a=a+1
+print("a=a+1 -->", a)
+a+=1
+print("a+=1 -->", a)
+a*=2
+print("a*=2 -->", a)
+a/=2
+print("a/=2 -->", a)
+a-=2
+print("a-=1 -->", a)
+a**=2
+print("a**=1 -->", a)
